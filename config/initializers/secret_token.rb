242e0afa09f54b89cdd440c1bd5f1938c0cf4426
Robson Marques <robsonmarques@gmail.com>
2012-12-06 23:27:40 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mascavo::Application.config.secret_token = '924d5d8ace2de631c4674c3dc9d20701d8f6c5f77b5ae904c05260387803ed18ea4dfbfc5c73eaca9d73d730545f8a1bdd4e40b097b7cfebf57330044c3ebc5f'
