f642f9c464ea4a8a5c8534c91a66a9555dda5bf0
Robson Marques <robsonmarques@gmail.com>
2013-02-04 13:13:43 -0200

    Remove deleted files

--- app/models/professional.rb
+++
@@ -1,8 +0,0 @@
-class Professional < ActiveRecord::Base
-
-  attr_accessible :position, :primary_contact
-
-  belongs_to :company
-  belongs_to :contact
-
-end
--- app/models/professional_info.rb
+++
@@ -1,7 +0,0 @@
-class ProfessionalInfo < ActiveRecord::Base
-
-  attr_accessible :position, :primary_contact
-
-  belongs_to :company
-
-end
